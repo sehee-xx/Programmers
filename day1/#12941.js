6b77df63ece68b9b77ca99a4f60cd6ca31d1fdb9
sehee-xx <didtpgml0627@gmail.com>
2022-10-10 20:34:45 +0900

    day1 폴더로 파일 이동

---
+++ day1/#12941.js
@@ -0,0 +1,13 @@
+// 최솟값 만들기
+
+function solution(A, B) {
+  var answer = 0;
+
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+
+  for (let i = 0; i < A.length; i++) {
+    answer += A[i] * B[i];
+  }
+  return answer;
+}
